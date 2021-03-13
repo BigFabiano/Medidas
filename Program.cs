dc05677de6a055ceec81482f99a899a08d6590b7
FabianoEtec <nbscbs@gmail.com>
2021-03-12 22:12:37 -0300

    Medidas finalizado!!!

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Medidas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String entrada;
+            double metros, centimetros, quilometros;
+
+            Console.Write("Entre com a medida (em metros): ");
+            entrada = Console.ReadLine();
+
+            metros = Convert.ToDouble(entrada);
+            centimetros = metros * 100;
+            quilometros = metros / 1000;
+
+            Console.WriteLine ("\n--- Equivalencia ---");
+            Console.WriteLine($"{centimetros:N2} CM");
+            Console.WriteLine($"{metros:N1} M");
+            Console.WriteLine($"{quilometros:N3} KM");
+
+            
+        }
+    }
+}
